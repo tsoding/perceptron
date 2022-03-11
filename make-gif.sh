b297011644818acfeb9915e86e9b28ffdfeaee06
rexim <reximkut@gmail.com>
2022-03-12 02:09:54 +0700

    Add make-gif.sh

---
+++ make-gif.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -xe
+
+./build.sh
+./main
+ffmpeg -y -i data/weights-%03d.ppm demo.mp4
+ffmpeg -y -i data/demo.mp4 -vf palettegen data/palette.png
+ffmpeg -y -i data/demo.mp4 -i data/palette.png -filter_complex paletteuse demo.gif
