664418691c6e682f93ee81ab290fde4b05191f57
rexim <reximkut@gmail.com>
2022-03-12 03:39:58 +0700

    Fix make-gif.sh

--- make-gif.sh
+++ make-gif.sh
@@ -4,6 +4,6 @@ set -xe
 
 ./build.sh
 ./main
-ffmpeg -y -i data/weights-%03d.ppm demo.mp4
+ffmpeg -y -i data/weights-%03d.ppm data/demo.mp4
 ffmpeg -y -i data/demo.mp4 -vf palettegen data/palette.png
 ffmpeg -y -i data/demo.mp4 -i data/palette.png -filter_complex paletteuse demo.gif
